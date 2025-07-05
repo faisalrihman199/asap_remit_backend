3035eea156c84d693d74cec0a98244d537d71f44
Faisal Rehman <faisalrihan199@gmail.com>
2025-07-05 15:44:09 +0500

    cors applied

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const cors = require('cors'); // <-- added
+
 const { sequelize } = require('./models');
 
 const app = express();
 
+// === Middleware ===
+app.use(cors()); // <-- added here
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('dev'));
 
-// Route imports
+// === Routes ===
 app.use('/api', require('./routes'));
 
-
-// Error Handler
+// === Error Handler ===
 const { errorHandler } = require('./middleware/errorHandler');
 app.use(errorHandler);
 
